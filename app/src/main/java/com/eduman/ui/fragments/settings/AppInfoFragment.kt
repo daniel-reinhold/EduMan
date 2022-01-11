30d18a6e462037d3bc058d605534a570dc25dd2b
Danie Reinhold <daniel.reinhold261201@gmail.com>
2022-01-11 18:44:18 +0100

    Implemented view for App information

--- app/src/main/java/com/eduman/ui/fragments/settings/AppInfoFragment.kt
+++ app/src/main/java/com/eduman/ui/fragments/settings/AppInfoFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.eduman.BuildConfig
 import com.eduman.R
 import com.eduman.core.EduManFragment
+import com.google.android.material.textview.MaterialTextView
 
 class AppInfoFragment : EduManFragment("AppInfoFragment") {
+
+    private var textViewVersion: MaterialTextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +20,18 @@ class AppInfoFragment : EduManFragment("AppInfoFragment") {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_app_info, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initialize()
+    }
+
+    private fun initialize() {
+        textViewVersion = activity?.findViewById(R.id.fragmentAppInfoTextViewVersion)
+
+        textViewVersion?.text = BuildConfig.VERSION_NAME
     }
 
 }
